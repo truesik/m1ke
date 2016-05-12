1ad498ea3c574560e3d88b412e3f1f74f13fd08e
truesik <truesik@gmail.com>
2016-05-12 20:31:27 +0300

    тесты

---
+++ src/test/java/org/j2overhead/m1ke/EngineTest.java
@@ -0,0 +1,23 @@
+package org.j2overhead.m1ke;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EngineTest {
+    @Test
+    public void createProgramFolder() throws Exception {
+
+    }
+
+    @Test
+    public void getFilesFromFolder() throws Exception {
+
+    }
+
+    @Test
+    public void createMasterBranch() throws Exception {
+
+    }
+
+}
