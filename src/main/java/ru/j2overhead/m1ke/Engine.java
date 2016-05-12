e6fa802c66d5dcc0c280220eaa1f5ada3d71941d
truesik <truesik@gmail.com>
2016-05-12 18:19:00 +0300

    Хер пойми что

---
+++ src/main/java/ru/j2overhead/m1ke/Engine.java
@@ -0,0 +1,28 @@
+package ru.j2overhead.m1ke;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Engine {
+    public Engine() {
+
+    }
+
+    public void createProgramFolder(File pathToFolder) {
+        if (new File(pathToFolder + "/.m1ke/branches").mkdirs() && new File(pathToFolder + "./m1ke/system").mkdirs()) {
+            System.out.println("Program folders is created");
+        } else {
+            System.out.println("Failed to create folders");
+        }
+    }
+
+    public List<File> getFilesFromFolder(File pathToFolder) {
+        File[] files = pathToFolder.listFiles();
+        if (files != null) {
+            return new ArrayList<>(Arrays.asList(files));
+        }
+        return new ArrayList<>();
+    }
+}
