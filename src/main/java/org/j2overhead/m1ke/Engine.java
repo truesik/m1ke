cd5e869a364838e7c9645889639ae17a912805d8
truesik <truesik@gmail.com>
2016-05-12 20:31:11 +0300

    странный метод

--- src/main/java/org/j2overhead/m1ke/Engine.java
+++ src/main/java/org/j2overhead/m1ke/Engine.java
@@ -1,12 +1,21 @@
 package org.j2overhead.m1ke;
 
-import java.io.File;
+import java.io.*;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Engine {
-    public Engine() {
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
+public class Engine {
+    private final static String DEFAULT_BRANCH = "/.m1ke/branches/master/";
+    private File path;
+
+    public Engine(File path) {
+
+        this.path = path;
     }
 
     public void createProgramFolder(File pathToFolder) {
@@ -29,4 +38,16 @@ public class Engine {
         }
         return fileList;
     }
+
+    public void createMasterBranch() {
+        for (File file : getFilesFromFolder(path)) {
+            try {
+                Files.copy(Paths.get(file.getPath()), Paths.get(path + DEFAULT_BRANCH), REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
 }
