fe06fc7e2676207652408a409a31629eba9c9587
truesik <truesik@gmail.com>
2016-05-14 14:54:13 +0300

    come back

---
+++ src/main/java/org/j2overhead/m1ke/utils/FileUtils.java
@@ -0,0 +1,106 @@
+package org.j2overhead.m1ke.utils;
+
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+    public static boolean compareTwoFiles(File oldFile, File newFile) {
+        try {
+            FileInputStream oldFileInputStream = new FileInputStream(oldFile);
+            DataInputStream oldDataInputStream = new DataInputStream(oldFileInputStream);
+            FileInputStream newFileInputStream = new FileInputStream(newFile);
+            DataInputStream newDataInputStream = new DataInputStream(newFileInputStream);
+            byte oldFileBytes = oldDataInputStream.readByte();
+            byte newFilesBytes = newDataInputStream.readByte();
+            if (oldFileBytes == newFilesBytes) {
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean compareTwoFilesByHash(File oldFile, File newFile) {
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            InputStream oldFileInputStream = Files.newInputStream(Paths.get(oldFile.getPath()));
+            InputStream newFileInputStream = Files.newInputStream(Paths.get(newFile.getPath()));
+            DigestInputStream oldFileDigestInputStream = new DigestInputStream(oldFileInputStream, messageDigest);
+            DigestInputStream newFileDigestInputStream = new DigestInputStream(newFileInputStream, messageDigest);
+            messageDigest.digest();
+        } catch (NoSuchAlgorithmException | IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static List<File> getFilesFromFolder(File pathToFolder) {
+        List<File> fileList = new ArrayList<>();
+        File[] files = pathToFolder.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    fileList.add(file);
+                }
+            }
+        }
+        return fileList;
+    }
+
+    public static List<File> getBranches(File file) {
+        return null;
+    }
+
+    public static void saveFileTree(String comment) {
+        String userDir = System.getProperty("user.dir");
+        class MyFileFindVisitor extends SimpleFileVisitor<Path> {
+
+            public FileVisitResult visitFile(Path path, BasicFileAttributes fileAttributes) {
+                find(path);
+                return FileVisitResult.CONTINUE;
+            }
+
+            public FileVisitResult preVisitDirectory(Path path, BasicFileAttributes fileAttributes) {
+                find(path);
+                return FileVisitResult.CONTINUE;
+            }
+
+            private PathMatcher matcher;
+            private MyFileFindVisitor(String pattern) {
+                try {
+                    matcher = FileSystems.getDefault().getPathMatcher(pattern);
+                } catch (IllegalArgumentException iae) {
+                    System.err.println("Invalid pattern; did you forget to prefix \"glob:\" or \"regex:\"?");
+                    System.exit(1);
+                }
+            }
+
+            private void find(Path path) {
+                Path name = path.getFileName();
+                if (matcher.matches(name)) {
+                    if (Files.isDirectory(path)) {
+                        System.out.println("Save directory:" + path.getFileName());
+                    } else {
+                        System.out.println("Save file:" + path.getFileName());
+                    }
+                }
+            }
+        }
+
+        try {
+            String pattern = "glob:*";
+            System.out.println("File list: ");
+            Files.walkFileTree(Paths.get(userDir), new MyFileFindVisitor(pattern));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Save " + userDir + (comment.equals("") ? "" : " with comment: " + comment));
+    }
+}
---
+++ src/main/java/org/j2overhead/m1ke/utils/LastOpenedBranchPropertyService.java
@@ -0,0 +1,58 @@
+package org.j2overhead.m1ke.utils;
+
+import java.io.*;
+import java.util.Properties;
+
+public class LastOpenedBranchPropertyService {
+    private static final String SYSTEM_PATH = ".m1ke" + File.separator + "system";
+    private static final String LAST_OPENED_BRANCH = "last-opened-branch.property";
+
+    private static LastOpenedBranchPropertyService instance;
+
+    public static LastOpenedBranchPropertyService getInstance() {
+        if (instance == null) {
+            instance = new LastOpenedBranchPropertyService();
+        }
+        return instance;
+    }
+
+    private LastOpenedBranchPropertyService() {
+    }
+
+    public String readLastOpenedBranch(String pathOfIntegratedFolder) {
+        Properties properties = new Properties();
+        try (InputStream inputStream = new FileInputStream(pathOfIntegratedFolder + File.separator + SYSTEM_PATH + LAST_OPENED_BRANCH)) {
+            properties.load(inputStream);
+            if (properties.containsKey("LAST_OPENED_BRANCH")) {
+                return properties.getProperty("LAST_OPENED_BRANCH");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public void writeLastOpenedBranch(String name, String pathOfIntegratedFolder) {
+        Properties properties = new Properties();
+        try (OutputStream outputStream = new FileOutputStream(pathOfIntegratedFolder + File.separator + SYSTEM_PATH + LAST_OPENED_BRANCH)) {
+            properties.setProperty("LAST_OPENED_BRANCH", name);
+            properties.store(outputStream, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void createLastOpenedBranchFile(String pathOfIntegratedFolder) {
+        File file = new File(pathOfIntegratedFolder + File.separator + SYSTEM_PATH + LAST_OPENED_BRANCH);
+        try {
+            if (file.createNewFile()) {
+                System.out.println("File is created");
+            } else {
+                System.out.println("File already exists");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
