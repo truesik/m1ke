0f4717005699fdb2895b433fb7cc58f570b0f599
AlexeyLevin <lay-way@ya.ru>
2016-05-15 13:50:51 +0300

    Add logger to testing, create and implements some simple test methods and create testData class

--- src/test/java/org/j2overhead/m1ke/TestData.java
+++ src/test/java/org/j2overhead/m1ke/TestData.java
@@ -1,9 +1,20 @@
 package org.j2overhead.m1ke;
 
+import org.j2overhead.m1ke.command.Commands;
+
 import java.io.File;
 
 public class TestData {
-   public static void createTestFile(){
+   public static String TEST_FILE_PATH = "D:" + File.separator +"test";
+   public static String TEST_FILE_NAME = "test.txt";
+   public static String INIT = Commands.INIT.toString();
+   public static String INTEGRATE = Commands.INTEGRATE.toString();
+   public static String CREATE_BRANCH = Commands.CREATE_BRANCH.toString();
+   public static String SAVE = Commands.SAVE.toString();
+   public static String REMOVE_BRANCH = Commands.REMOVE_BRANCH.toString();
+   public static String GET_BRANCH = Commands.GET_BRANCH.toString();
+   public static String QUIT = Commands.QUIT.toString();
 
+   public static void main(String[] args) {
    }
 }
