05d2c73369f5d2561d1410682fba21ed0ffabcf3
AlexeyLevin <lay-way@ya.ru>
2016-05-15 13:43:56 +0300

    Enum change

--- src/main/java/org/j2overhead/m1ke/command/Commands.java
+++ src/main/java/org/j2overhead/m1ke/command/Commands.java
@@ -1,11 +1,20 @@
 package org.j2overhead.m1ke.command;
 
 public enum Commands {
-    INIT,
-    INTEGRATE,
-    SAVE,
-    CREATE_BRANCH,
-    REMOVE_BRANCH,
-    GET_BRANCH,
-    QUIT
+    INIT("init"),
+    INTEGRATE("integrate"),
+    SAVE("save"),
+    CREATE_BRANCH("create-branch"),
+    REMOVE_BRANCH("remove-branch"),
+    GET_BRANCH("get-branch"),
+    QUIT("quit");
+
+    private final String name;
+    Commands(String s) {
+        name = s;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
